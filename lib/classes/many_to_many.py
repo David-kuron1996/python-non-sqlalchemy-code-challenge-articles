ff255b5c25912593975b187b61b685e07f8846a8
David <davidkuron1996@gmail.com>
2025-11-17 12:25:02 +0300

    add this file

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,36 +3,49 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
 class Author:
     def __init__(self, name):
         self.name = name
+        self.authored_articles = []  # Track articles by this author
 
     def articles(self):
-        pass
+        return self.authored_articles
 
     def magazines(self):
-        pass
+        # Get unique magazines the author has written for
+        return list(set(article.magazine for article in self.authored_articles))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self.authored_articles.append(article)
+        magazine.magazine_articles.append(article)  # Add to magazine's articles
 
     def topic_areas(self):
-        pass
+        # Get unique categories of magazines the author has written for
+        return list(set(article.magazine.category for article in self.authored_articles))
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.magazine_articles = []  # Track articles in this magazine
 
     def articles(self):
-        pass
+        return self.magazine_articles
 
     def contributors(self):
-        pass
+        # Get unique authors who have written for this magazine
+        return list(set(article.author for article in self.magazine_articles))
 
     def article_titles(self):
-        pass
+        # Get titles of all articles in this magazine
+        return [article.title for article in self.magazine_articles]
 
     def contributing_authors(self):
-        pass
+        # Get authors with more than two articles in this magazine
+        author_counts = {}
+        for article in self.magazine_articles:
+            author = article.author
+            author_counts[author] = author_counts.get(author, 0) + 1
+        return [author for author, count in author_counts.items() if count > 1]
