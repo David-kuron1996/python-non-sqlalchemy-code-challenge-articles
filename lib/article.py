6a2e902e98539e1ab6a65d565cbd0d96de670128
David <davidkuron1996@gmail.com>
2025-11-17 12:13:28 +0300

    add this file

---
+++ lib/article.py
@@ -0,0 +1,20 @@
+class Article:
+    all = []
+    
+    def __init__(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        Article.all.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Title must be a string")
+        if not (5 <= len(value) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters")
+        self._title = value
